b0957129a67ec418d24b3b22a1adefabcbb51f93
elmurod <elmurodvkh@gmail.com>
2024-01-14 00:19:46 +0500

    empty example

---
+++ js/script3.js
@@ -0,0 +1,149 @@
+// Integer1 ------------------------
+
+let L = 320;
+
+let m = L / 100;
+
+console.log(parseInt(m));
+
+// Integer2 -------------------------
+
+let M  = 1002;
+
+let fullt = M / 1000;
+
+console.log(parseInt(fullt));
+
+// Interger3 ------------------------
+
+let bayt = 3020;
+
+let kbayt = bayt / 1024;
+
+console.log(parseInt(kbayt));
+
+// Interger4 -------------------------
+
+let A = 5;
+
+let B = 2;
+
+let AB = A / B;
+
+console.log(AB);
+
+// Interger5 --------------------------
+
+let a = 10;
+
+let b = 5;
+
+let ab = a / b;
+
+console.log(ab)
+
+// Interger6 ---------------------
+
+let ikkixonali = 85;
+
+let onlik = ikkixonali / 10;
+
+let birlik = ikkixonali % 10;
+
+console.log(parseInt(onlik));
+
+console.log(birlik);
+
+// Interger7 -----------------------------
+
+let berilgan = 45, bir1 = berilgan / 10, birson = parseInt(bir1), ikkison = berilgan % 10;
+
+let generalY = birson + ikkison;
+
+console.log(birson);
+
+console.log(ikkison);
+
+console.log(generalY);
+
+// Interger8 ----------------------------
+
+let org = 74, num1 = org / 10, num2 = org  % 10;
+
+let tes = num2 * 10 + parseInt(num1)
+
+console.log(org);
+
+console.log(tes);
+
+// Interger9 ---------------------------
+
+let uchxonali = 245, yuz = uchxonali / 100, qol = parseInt(yuz), generalX = qol * 100;
+
+console.log(generalX);
+
+// Interger10 ---------------------------
+
+let uch = 524;
+
+let bir = uch % 10;
+
+let ten = uch % 100, ten1 = ten / 10, qol1 = parseInt(ten1), generalG = qol1 * 10;
+
+console.log(parseInt(bir));
+
+console.log(generalG)
+
+// Interger11 --------------------------
+
+let ber = 158, bb = parseInt(ber / 100), ii = ber % 100, ii2 = parseInt(ii / 10), uu = ber % 10;
+
+let generalU = bb + ii2 + uu;
+
+console.log(generalU);
+
+// Interger12 -------------------------
+
+let uchx = 154, per = uchx % 10, per1 = uchx % 100, per11 = parseInt(per1 / 10), per2 = parseInt(uchx / 100);
+
+console.log(per, per11, per2);
+
+// Interger13 -------------------------
+
+let uchb = 767, del = uchb % 10, vari = uchb % 100;
+
+console.log(vari, del);
+
+// Interger14 -------------------------
+
+// Interger15 ------------------------
+
+// Interger16 ------------------------
+
+// Interger17 ------------------------
+
+// Interger18 ------------------------
+
+// Interger19 ------------------------
+
+let kunb = 120, tolliq = kunb / 60;
+
+console.log(tolliq);
+
+// Interger20 -------------------
+
+let kunN = 320, tolliqS = kunN / 3600;
+
+console.log(tolliqS);
+
+// Interger21 --------------------
+
+let Nsek = 656, Nmin = Nsek / 60;
+
+console.log(Nmin);
+
+// 20 misol -----------------------
+
+let uchxona = 754, yuzlik = parseInt(uchxona / 100), birlikk = uchxona % 100, birlikk1 = parseInt(birlikk / 10), onlikk = parseInt(uchxona % 10);
+
+console.log(yuzlik !== onlikk && onlikk !== birlikk1 && yuzlik !== birlikk1); 
